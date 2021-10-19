933293ce5e6fde6c56546cf7d192f1908276f440
Ryo Suzuki <ryosuzk@googlemail.com>
2021-10-19 17:42:01 -0600

    Video conferencing call

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.static(__dirname))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
+app.get('/local', (req, res) => {
+  res.sendFile(path.join(__dirname + '/local.html'))
+})
+
+
 
 server.listen(3000, () => {
   console.log('listening 3000')
