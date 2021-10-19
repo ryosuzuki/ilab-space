48cf671ffc4b2d5a040963b5fb1c336dea26e14e
Ryo Suzuki <ryosuzk@googlemail.com>
2021-10-19 13:15:04 -0600

    Add obj file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const path = require('path')
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+server.listen(3000, () => {
+  console.log('listening 3000')
+})
